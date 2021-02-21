b60bbaa01f3d9a3fd38be4237b73132167f3ba9c
Damian Yerrick <git@pineight.com>
2021-02-21 12:39:12 -0500

    NES: display stage number

--- nes/src/continue.s
+++ nes/src/continue.s
@@ -6,6 +6,9 @@
 ;
 ; 00-37 UI tiles from continuetiles.png
 
+NUM_STAGES = 3
+STAGE_SEL_POS = $2000+11+32*10
+
 TILE_BORDER = $00
 TILE_BLANK = $01
 TILE_TLCORNER = $02
@@ -22,10 +25,27 @@ TILE_OKSYMBOL = $30
 TILE_NGSYMBOL = $33
 TILE_LARROW_DISABLED = $36
 TILE_RARROW_DISABLED = $37
+TILE_STAGE_ONES = $40
+TILE_STAGE_TENS0 = $4A
+TILE_STAGE_TENS1 = $4B
+TILE_STAGE_LABEL = $4C
+
+; 00-3F: UI tiles
+; 40-49: digits 0-9
+; 4A:    current stage - 1
+; 4B:    current stage
+; 4C-4F: "Stage"
+
+.bss
+cur_stage: .res 1
+titles_draw_progress: .res 1
 
 .code
 
 .proc continue_main
+  lda #1
+  sta cur_stage
+
   jsr popslide_init
   lda #VBLANK_NMI
   sta PPUCTRL
@@ -37,8 +57,6 @@ TILE_RARROW_DISABLED = $37
   jsr ppu_clear_nt
 
   ; TODO: Color logo
-  ; TODO: Draw stage number
-  ; TODO: Draw digits
   ; TODO: Set up VWF canvas for coin titles on this page
   ; TODO: Draw coin titles on this page
   ; TODO: Left and Right to choose a page
@@ -54,6 +72,44 @@ TILE_RARROW_DISABLED = $37
   :
     cmp nmis
     beq :-
+  jsr popslide_terminate_blit
+
+  ; Load digits
+  jsr clearLineImg
+  lda #9
+  digitloop:
+    pha
+    asl a
+    asl a
+    sec
+    rol a
+    tax  ; 1, 9, 17, 23, ...
+    pla
+    pha
+    ora #'0'
+    jsr vwfPutTile
+    pla
+    sec
+    sbc #1
+    bpl digitloop
+  ldx #0
+  lda #TILE_STAGE_ONES
+  ldy #$FA
+  jsr nstripe_2bpp_from_lineImg
+  jsr popslide_terminate_blit
+
+  ; Write "Stage"
+  jsr clearLineImg
+  ldx #0
+  lda #>word_Stage
+  ldy #<word_Stage	
+  jsr vwfPuts
+  lda #TILE_STAGE_LABEL
+  clc
+  ldx #0
+  ldy #%11110100
+  jsr nstripe_2bpp_from_lineImg
+  jsr draw_stage_number
   jsr popslide_terminate_blit
 
 loop:
@@ -73,12 +129,77 @@ loop:
   rts
 .endproc
 
+STAGE_ARROWS_POS = $2000+7+32*10
+
+.proc draw_stage_number
+  ; Draw digit used for x1-x9
+  lda #0
+  sta titles_draw_progress
+  jsr clearLineImg
+  lda cur_stage
+  beq is_stage1
+    ora #'0'
+    ldx #4
+    jsr vwfPutTile
+  is_stage1:
+
+  ; Draw digit used for x0 and stage number
+  lda cur_stage
+  cmp #9
+  bcc not_stage10
+    lda #'1'
+    ldx #7
+    jsr vwfPutTile
+    clc
+    lda #$FF
+  not_stage10:
+  adc #'1'
+  ldx #12
+  jsr vwfPutTile
+
+  ; Write these digits to VRAM
+  ldy #$F2
+  lda #TILE_STAGE_TENS0
+  ldx #4
+  jsr nstripe_2bpp_from_lineImg
+
+  ; Add left/right arrow control
+  ldx popslide_used
+  txa
+  clc
+  adc #6
+  sta popslide_used
+  lda #>STAGE_SEL_POS
+  sta popslide_buf+0,x
+  lda #<STAGE_SEL_POS
+  sta popslide_buf+1,x
+  lda #3-1
+  sta popslide_buf+2,x
+  lda cur_stage
+  cmp #1
+  lda #TILE_LARROW
+  bcs :+
+    lda #TILE_LARROW_DISABLED
+  :
+  sta popslide_buf+3,x
+  lda #TILE_STAGE_TENS1
+  sta popslide_buf+4,x
+  lda cur_stage
+  cmp #NUM_STAGES-1
+  lda #TILE_RARROW
+  bcc :+
+    lda #TILE_RARROW_DISABLED
+  :
+  sta popslide_buf+5,x
+  rts
+.endproc
+
 .rodata
 continue_stripes:
   ; Palette
   .dbyt $3F00
   .byte 32-1
-  .byte $0F,$1A,$2A,$20, $0F,$00,$10,$20, $0F,$00,$10,$20, $0F,$00,$10,$20
+  .byte $0F,$1A,$2A,$20, $0F,$00,$10,$20, $0F,$3C,$0F,$3C, $0F,$0F,$32,$32
   .byte $0F,$00,$10,$20, $0F,$00,$10,$20, $0F,$00,$10,$20, $0F,$00,$10,$20
 
   ; Border
@@ -102,9 +223,11 @@ continue_stripes:
   .byte 18-1
   .byte $28,$17,$2B,$25,$26,$27,$28,$29,$2A,$2B,$2C,$2D,$2E,$2F,$28,$29,$2A,$2B
   .dbyt $2000+7+32*10
-  .byte 7-1
-  .byte $4C,$4D,$4E,$01,$20,$4B,$21
+  .byte 4-1
+  .byte $4C,$4D,$4E,$4F
 
-  ; 70 left. What else fits?
+  ; 73 left. What else fits?
   .byte $FF
 
+word_Stage:
+  .byte "Stage", 0
--- nes/src/vwf_draw.s
+++ nes/src/vwf_draw.s
@@ -15,14 +15,17 @@
 ; 2015-02: vwfPutTile rewritten by DY again; no more Blargg code
 
 .include "nes.inc"
+.include "popslide.inc"
 .export vwfPutTile, vwfPuts, vwfPuts0
 .export vwfGlyphWidth, vwfStrWidth, vwfStrWidth0
 .export clearLineImg, lineImgBuf, invertTiles
-.export copyLineImg_helpscreen
+.export nstripe_2bpp_from_lineImg, nstripe_1bpp_from_lineImg
 .exportzp lineImgBufLen
 .import vwfChrData, vwfChrWidths
 
-lineImgBuf = $0100
+; Overlap VWF line image and shadow OAM to stay out of popslide's way
+; If we completely rewrite shadow OAM every frame, it's OK
+lineImgBuf = $0200
 lineImgBufLen = 128
 FONT_HT = 8
 MIN_CODEUNIT = $20
@@ -278,32 +281,114 @@ invertloop:
 .endproc
 
 ;;
+; @param X source position
+; @param C:A tile number in $0000
+; @param Y[3:0] tile count
+; @param Y[7:4] AND and XOR masks for colorization
+.proc nstripe_2bpp_from_lineImg
+vramdst = $00
+srcpos = $02
+tilecount = $03
+and0 = $04
+xor0 = $05
+and1 = $06
+xor1 = $07
+  rol a
+  rol a
+  rol a
+  rol a
+  pha
+  and #$F0
+  sta vramdst+0
+  pla
+  rol a
+  and #$1F
+  sta vramdst+1
+  txa
+  and #$F8
+  sta srcpos
+  tya
+  and #$0F
+  sta tilecount
+  tya
+  and #$F0
+  ldx #3
+  masksloop:
+    ldy #0
+    asl a
+    bcc :+
+      dey
+    :
+    sty and0,x
+    dex
+    bpl masksloop
+
+  ldy popslide_used
+  ldx srcpos
+  packetloop:
+    lda vramdst+0
+    sta popslide_buf+1,y
+    lda tilecount
+    and #3
+    bne :+
+      lda #4
+    :
+    asl a
+    asl a
+    asl a
+    asl a
+    adc #$FF
+    sta popslide_buf+2,y
+    sec
+    adc vramdst+0
+    sta vramdst+0
+    lda vramdst+1
+    sta popslide_buf+0,y
+    adc #0
+    sta vramdst+1
+    iny
+    iny
+    iny
+    tileloop:
+    sliverloop:
+      lda lineImgBuf+0,x
+      and and0
+      eor xor0
+      sta popslide_buf+0,y
+      lda lineImgBuf+0,x
+      and and1
+      eor xor1
+      sta popslide_buf+8,y
+      iny
+      inx
+      txa
+      and #$07
+      bne sliverloop
+      tya
+      clc
+      adc #8
+      tay
+      dec tilecount
+      lda tilecount
+      beq done
+      and #3
+      bne tileloop
+    sta $4444
+    jmp packetloop
+  done:
+  sty popslide_used
+
+  rts
+.endproc
+
+;;
 ; Copies a rendered line of text to CHR RAM, packing 2 8x8 pixel
 ; tiles into each plane.
 ; Plane 0 contains tiles 0, 1, 4, 5, 8, 9, 12, and 13.
 ; Plane 1 contains tiles 2, 3, 6, 7, 10, 11, 14, and 15.
 ; in:  AAYY = destination address ($0000-$1F80)
-.proc copyLineImg_helpscreen
-  sta PPUADDR
-  sty PPUADDR
-  ldx #0
-  clc
-partloop:
-  .repeat 2, plane
-    .repeat 2, pair
-      .repeat 8, scanline  
-        lda lineImgBuf+8*plane+16*pair+scanline,x
-        sta PPUDATA
-      .endrepeat
-    .endrepeat
-  .endrepeat
-  txa
-  adc #32
-  tax
-  cpx #lineImgBufLen
-  bcs done
-  jmp partloop
-done:
+.proc nstripe_1bpp_from_lineImg
+  ; TODO
   rts
 .endproc
 
