8b854791383ce88799ff727391c123aaf88789ba
Damian Yerrick <pino@pineight.com>
2022-12-11 16:05:43 -0500

    GB coin 29: detect joypad interrupt
    also work around Super Game Boy's bridge chip interpreting
    selecting both halves of the key matrix as a packet start

--- gameboy/mk.sh
+++ gameboy/mk.sh
@@ -44,4 +44,4 @@ objlisto=$(printf "obj/gb/%s.o " $objlist $genobjlist)
 rgblink -o "$title.gb" -p 0xFF -m "$title.map" -n "$title.sym" $objlisto
 
 # per beware: don't add -s until SGB-related tests are ready
-rgbfix -jvt "$inttitle" -l0x33 -m0 -n0 -p0xFF -r0 "$title.gb"
+rgbfix -jvt "$inttitle" -l0x33 -m0 -n0 -p0xFF -r0 -s "$title.gb"
